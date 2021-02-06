684ae10f29c18d9fdf6a28e26e1ed8d6aa658287
k-bamboo-cyber <olesya1234562009@rambler.ru>
2021-02-06 12:36:41 +0300

    https://github.com/berpress/shop_tests/issues/46

--- pages/my_credit_slips.py
+++ pages/my_credit_slips.py
@@ -5,21 +5,14 @@ class MyCreditSlipsPage:
     def __init__(self, app):
         self.app = app
 
-    def my_credit_slips_button(self):
+    def credit_slips_button(self):
         return self.app.driver.find_element(*MyCreditSlipsLocators.CREDIT_SLIPS_BUTTON)
 
-    def my_credit_slips_button_click(self):
-        self.my_credit_slips_button().click()
+    def credit_slips_button_click(self):
+        self.credit_slips_button().click()
 
-    def check(self):
-        """
-        Функция которая кликает по кнопке 'my credit slips',
-        добавлена для читабельности теста
-        """
-        self.my_credit_slips_button_click()
-
-    def my_c_s_field(self):
+    def header(self):
         return self.app.driver.find_element(*MyCreditSlipsLocators.CREDIT_SLIPS_TEXT)
 
     def my_c_s_field_text(self):
-        return self.my_c_s_field().text
+        return self.header().text
