eee224d115a20f4a5c62c31f575f63f6ea1f573f
nsh12 <ansh120022@gmail.com>
2021-02-06 22:17:23 +0300

    fix issues

--- tests/test_my_orders.py
+++ tests/test_my_orders.py
@@ -9,7 +9,10 @@ class TestMyOrders:
     def test_my_orders(self, app, login):
         """
         1. Открыть страницу
-        2. Кликнуть на кнопку My Orders
+        2. Кликнуть на ссылку My Orders в футере
+        3. Открылась страница заказов
+        4. Нажать на кнопку выхода в правом вернем углу
+        5. Происходит разлогин
         """
         app.my_orders.check()
         assert app.my_orders.my_o_field_text() == MyOrdersPage.ORDER_HISTORY
